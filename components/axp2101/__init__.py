649c948b2924d01cc08e7de7671b8b2de3807094
285165 <zsb@me.com>
2025-08-25 14:39:55 +0200

    Update __init__.py

--- components/axp2101/__init__.py
+++ components/axp2101/__init__.py
@@ -2,8 +2,9 @@ import esphome.codegen as cg
 cg.add_library(
     name="levisxhe/XPowersLib",
     repository="https://github.com/lewisxhe/XPowersLib.git",
-    version=None,
+    version=v0.3.0,
 )
+
 
 
 
