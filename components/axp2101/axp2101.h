21cceae9f4f77b0d95e9d69dd9bc81ff989853d1
285165 <zsb@me.com>
2025-09-01 20:23:41 +0200

    Update axp2101.h

--- components/axp2101/axp2101.h
+++ components/axp2101/axp2101.h
@@ -5,6 +5,7 @@
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/core/component.h"
+#include "esphome/core/gpio.h"
 
 #define XPOWERS_CHIP_AXP2101
 #include <XPowersLib.h>
